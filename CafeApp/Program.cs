210ae72c58f18a175448dc93fc69314f81bb2669
Emirhan Tuygun <114773586+emirhantuygun@users.noreply.github.com>
2023-12-24 17:23:42 +0300

    ViewBag Added

--- CafeApp/Program.cs
+++ CafeApp/Program.cs
@@ -36,8 +36,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
-app.UseRouting();
 app.UseSession();
+app.UseRouting();
 
 app.MapControllerRoute(
     name: "default",
